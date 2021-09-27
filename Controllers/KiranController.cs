2279b06a1b19c4790ce7557401bd2d0126156586
Kiran Kanakadas <kiran.kanakadas@navexglobal.com>
2021-09-27 18:42:44 +0530

    Add project files.

---
+++ Controllers/KiranController.cs
@@ -0,0 +1,38 @@
+﻿using FirstWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApplication.Controllers
+{
+    public class KiranController : Controller
+    {
+        public IActionResult Page()
+        {
+            var students = new List<Student>()
+            {
+                new Student
+                {
+                    Id = 1,
+                    Name = "Kiran",
+                    Marks = 90
+                },
+                new Student
+                {
+                    Id = 2,
+                    Name = "Justin",
+                    Marks = 95
+                }
+
+            };
+            return View(students);
+        }
+        public IActionResult Welcome()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ Views/Kiran/Page.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstWebApplication.Views.Kiran
+{
+    public class PageModel : Microsoft.AspNetCore.Mvc.RazorPages.PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
